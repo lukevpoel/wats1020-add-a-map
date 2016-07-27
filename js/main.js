8c5aa1a8729b8370156f910a10a949ca6147ff2c
Luke Vander Poel <airscout99@gmail.com>
2016-07-26 19:42:55 -0700

    fixed leaflet version issue
    Leaflet 1.0.0 was loading before the page with leaflet 0.7.3 loading
    after.  Broke the markers. Fixed and all markers functioning correctly

--- js/main.js
+++ js/main.js
@@ -40,36 +40,19 @@ $( document ).ready(function(){
   //open the map
   L.control.layers(mapLayers).addTo(map);
   satLayer.addTo(map);
-  /** tried another method for adding markers that wasn't working. Code is here in case I can work on it later
-  for ( var i=0; i < markers.length; ++i )
-  {
-     L.marker( [markers[i].lat, markers[i].lng] )
-        .bindPopup( '<a href="' + markers[i].desc + '" target="markers.js">' + markers[i].name + '</a>' )
-        .addTo( map );
-  }**/
 
   //Customize that Map to show markers with popups at no fewer than 3
   // interesting locations. (You'll need to figure out the latitude/longitude for
   // these locations using a mapping tool such as Google Maps.)
-  //var peakMarker = L.marker([46.852, -121.760]).addTo(map);
-  //peakMarker.bindPopup("Welcome to Mt. Rainier!</b><br>This peak is 14,392 feet high.");
 
-  var paradiseCircle = L.circle([46.786020, -121.734973], 250, {
-    color: 'red',
-    fillColor: 'green',
-    fillOpacity: 0.7,
-    opacity: 1
-  }).addTo(map);
-  paradiseCircle.bindPopup("Welcome to Paradise!</b><br>Paradise is the name of an area at approximately 5,400 feet on the south slope of Mount Rainier in Mount Rainier National Park in Washington, United States.");
 
-  var muirMarker = L.marker([46.835446, -121.731636]).addTo(map).bindPopup('Welcome');
+  var paradiseMarker = L.marker([46.786020, -121.734973]).addTo(map);
+  paradiseMarker.bindPopup("Welcome to Paradise!</b><br>Paradise is the name of an area at approximately 5,400 feet on the south slope of Mount Rainier in Mount Rainier National Park in Washington, United States.");
+
+  var muirMarker = L.marker([46.835446, -121.731636]).bindPopup('Welcome').addTo(map);
   muirMarker.bindPopup("Welcome to Camp Muir!</b><br>Camp Muir represents a dividing line: this is as high as you can go on Mount Rainier without a climbing permit. From here, you get a close-up view that few visitors to the park ever see: hanging glaciers, towering seracs, yawning crevasses, and thundering rockfall as the ridges of ash and breccia high above melt and crumble in the heat of the sun.");
 
-  var peakCircle = L.circle([46.852, -121.760], 500, {
-    color: 'green',
-    fillColor: 'red',
-    fillOpacity: 0.7,
-    opacity: 1
-  }).addTo(map);
-  peakCircle.bindPopup("Welcome to Mt. Rainier!</b><br>This peak is 14,392 feet high.");
+  var peakMarker = L.marker([46.852, -121.760]).addTo(map);
+  peakMarker.bindPopup("Welcome to Mt. Rainier!</b><br>This peak is 14,392 feet high.");
+
 });
