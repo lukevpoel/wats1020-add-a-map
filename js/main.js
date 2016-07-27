ab37f9f4063950131e379a64e375bd6b1d44d229
Luke Vander Poel <airscout99@gmail.com>
2016-07-26 19:01:38 -0700

    Changed to circles

--- js/main.js
+++ js/main.js
@@ -51,13 +51,25 @@ $( document ).ready(function(){
   //Customize that Map to show markers with popups at no fewer than 3
   // interesting locations. (You'll need to figure out the latitude/longitude for
   // these locations using a mapping tool such as Google Maps.)
-  var peakMarker = L.marker([46.852, -121.760]).addTo(map);
-  peakMarker.bindPopup("Welcome to Mt. Rainier!</b><br>This peak is 14,392 feet high.");
+  //var peakMarker = L.marker([46.852, -121.760]).addTo(map);
+  //peakMarker.bindPopup("Welcome to Mt. Rainier!</b><br>This peak is 14,392 feet high.");
 
-  var paradiseMarker = L.marker([46.786020, -121.734973]).addTo(map);
-  paradiseMarker.bindPopup("Welcome to Paradise!</b><br>Paradise is the name of an area at approximately 5,400 feet on the south slope of Mount Rainier in Mount Rainier National Park in Washington, United States.");
+  var paradiseCircle = L.circle([46.786020, -121.734973], 250, {
+    color: 'red',
+    fillColor: 'green',
+    fillOpacity: 0.7,
+    opacity: 1
+  }).addTo(map);
+  paradiseCircle.bindPopup("Welcome to Paradise!</b><br>Paradise is the name of an area at approximately 5,400 feet on the south slope of Mount Rainier in Mount Rainier National Park in Washington, United States.");
 
-  var muirMarker = L.marker([46.835446, -121.731636]).addTo(map);
-  paradiseMarker.bindPopup("Welcome to Camp Muir!</b><br>Camp Muir represents a dividing line: this is as high as you can go on Mount Rainier without a climbing permit. From here, you get a close-up view that few visitors to the park ever see: hanging glaciers, towering seracs, yawning crevasses, and thundering rockfall as the ridges of ash and breccia high above melt and crumble in the heat of the sun.");
+  var muirMarker = L.marker([46.835446, -121.731636]).addTo(map).bindPopup('Welcome');
+  muirMarker.bindPopup("Welcome to Camp Muir!</b><br>Camp Muir represents a dividing line: this is as high as you can go on Mount Rainier without a climbing permit. From here, you get a close-up view that few visitors to the park ever see: hanging glaciers, towering seracs, yawning crevasses, and thundering rockfall as the ridges of ash and breccia high above melt and crumble in the heat of the sun.");
 
+  var peakCircle = L.circle([46.852, -121.760], 500, {
+    color: 'green',
+    fillColor: 'red',
+    fillOpacity: 0.7,
+    opacity: 1
+  }).addTo(map);
+  peakCircle.bindPopup("Welcome to Mt. Rainier!</b><br>This peak is 14,392 feet high.");
 });
